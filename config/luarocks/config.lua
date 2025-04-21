dc64a18f8117fde3084bbab7fbd854b3a6bee650
AlterSabeh <betamaximum22@gmail.com>
2025-04-21 19:40:38 +0800

    sort variables

--- config/luarocks/config.lua
+++ config/luarocks/config.lua
@@ -1,7 +1,7 @@
+lua_interpreter = "lua";
 rocks_trees = {
   { root = os_etenv("LUAROCKS_TREE") },
 }
-lua_interpreter = "lua";
 variables = {
   LUA_DIR = os_getenv("LUAENV_ROOT") .. "/versions/current",
   LUA_INCDIR = os_getenv("LUAENV_ROOT") .. "/versions/current/include",
