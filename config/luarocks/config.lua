a92bd4db2b740038ac00b232e90f10972e7ac8dd
AlterSabeh <betamaximum22@gmail.com>
2025-04-21 19:34:21 +0800

    update luarocks configuration to use environment variable for rocks tree path

--- config/luarocks/config.lua
+++ config/luarocks/config.lua
@@ -1,9 +1,9 @@
 rocks_trees = {
-  { root = home .. "/.local/share/luarocks" },
+  { root = os_getenv("LUAROCKS_TREE") },
 }
 lua_interpreter = "lua";
 variables = {
-  LUA_DIR = os.getenv("LUAENV_ROOT") .. "/versions/current",
-  LUA_INCDIR = os.getenv("LUAENV_ROOT") .. "/versions/current/include",
-  LUA_BINDIR = os.getenv("LUAENV_ROOT") .. "/versions/current/bin",
+  LUA_DIR = os_getenv("LUAENV_ROOT") .. "/versions/current",
+  LUA_INCDIR = os_getenv("LUAENV_ROOT") .. "/versions/current/include",
+  LUA_BINDIR = os_getenv("LUAENV_ROOT") .. "/versions/current/bin",
 }
